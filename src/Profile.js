5ad7588f7acac4be1035d043ecd2c2ab612c55dc
wjyeop <wnsduq2178@gmail.com>
2022-10-23 12:15:43 +0900

    PROFILE 수정

--- src/Profile.js
+++ src/Profile.js
@@ -4,11 +4,23 @@ export const Profile = () => {
     return(
         <div id="body">
             <div id="profile">
+                <div>
+                    <h1>PROFILE</h1>
+                </div>
                 <div className="left">
                     <img src="img/good.png" alt=""/>
                 </div>
                 <div className="right">
-                    <p>안녕하세여</p>
+                    <p>안녕하세요 프론트엔드 개발자 원준엽입니다.</p>
+                    <ul>
+                        <li>이름 : 원준엽</li>
+                        <li>생년월일 : 96.10.11</li>
+                        <li>주소지 : 서울 강서구</li>
+                    </ul>
+                    <ul>
+                        <li>연락처 : 010 2482 1996</li>
+                        <li>이메일 : wnsduq2178@gmail.com</li>
+                    </ul>
                 </div>
             </div>
         </div>
