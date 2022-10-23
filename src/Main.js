d22dd8c270fedfe28a64c2e5c1f2da448ff911e0
wjyeop <wnsduq2178@gmail.com>
2022-10-23 15:51:30 +0900

    Skills 추가, 상단메뉴 고정

--- src/Main.js
+++ src/Main.js
@@ -3,12 +3,14 @@ import Footer from "./Footer";
 import './css/style.css';
 import './css/common.css';
 import Profile from "./Profile";
+import Skills from "./Skills";
 
 function Main() {
   return (
     <div>
       <Menu/>
       <Profile/>
+      <Skills/>
       <Footer/>
     </div>
   );
---
+++ src/Skills.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './css/style.css';
+import './css/common.css';
+
+
+export const Skills = () => {
+    return(
+        <div id="body">
+            <div id="skills">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Skills;
