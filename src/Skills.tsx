f514f1e4db92911dfa6bedd153aaba26039ee9fb
Wjyeop <wnsduq2178@gmail.com>
2023-06-05 22:16:09 +0900

    Skills 업뎃

--- src/Skills.tsx
+++ src/Skills.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
+
+const Skills = () => {
+  return (
+    <SkillsContainer id="skills">
+      <Title>Skills</Title>
+      <SkillsContent>
+        <JavaScript>
+          <JavaScriptImg src={process.env.PUBLIC_URL + '/javascript.svg'} alt="프로필 사진" />
+          <JavaScriptText>JavaScript</JavaScriptText>
+        </JavaScript>
+        <TypeScript>
+          <TypeScriptImg src={process.env.PUBLIC_URL + '/typescript.svg'} alt="프로필 사진" />
+          <TypeScriptText>TypeScript</TypeScriptText>
+        </TypeScript>
+        <JavaScript>
+          <JavaScriptImg src={process.env.PUBLIC_URL + '/javascript.svg'} alt="프로필 사진" />
+          <JavaScriptText>JavaScript</JavaScriptText>
+        </JavaScript>
+      </SkillsContent>
+    </SkillsContainer>
+  );
+}
 
 const SkillsContainer = styled.div`
-  width: 100%;
+  width: 90%;
   padding: 1rem;
   background-color: #f9f9f9;
   border-radius: 10px;
@@ -13,34 +35,46 @@ const SkillsContainer = styled.div`
 const Title = styled.h2`
   font-size: 1.2rem;
   margin-left: 1rem;
-`
+`;
 
 const SkillsContent = styled.div`
   flex-wrap: wrap;
   margin-left: 5rem;
 `;
 
-const Skills = () => {
-  return (
-    <SkillsContainer id="skills">
-      <Title>Skills</Title>
-      <SkillsContent>
-        {/* <span>주요 기술 : </span>
-                <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=plastic&logo=JavaScript&logoColor=white"/>
-                <img src="https://img.shields.io/badge/TypeScript-3178C6?style=plastic&logo=TypeScript&logoColor=white"/>
-                <img src="https://img.shields.io/badge/HTML-E34F26?style=plastic&logo=HTML&logoColor=white"/>
-                <img src="https://img.shields.io/badge/CSS-1572B6?style=plastic&logo=CSS&logoColor=white"/>
-                <img src="https://img.shields.io/badge/React-61DAFB?style=plastic&logo=React&logoColor=white"/>
-                <img src="https://img.shields.io/badge/Recoil-61DAFB?style=plastic&logo=Recoil&logoColor=white"/><br/><br/>
+const Skill = css`
+  width: 100%;
+  border-radius: 10px;
+  margin-bottom: 1rem;
+`;
 
-        <span>환경 : </span>
-                <img src="https://img.shields.io/badge/Github-181717?style=plastic&logo=Github&logoColor=white"/>
-                <img src="https://img.shields.io/badge/Postman-FF6C37?style=plastic&logo=Postman&logoColor=white"/>
-                <img src="https://img.shields.io/badge/Swagger-85EA2D?style=plastic&logo=Swagger&logoColor=white"/>
-                <img src="https://img.shields.io/badge/Figma-F24E1E?style=plastic&logo=Figma&logoColor=white"/>     */}
-      </SkillsContent>
-    </SkillsContainer>
-  );
-}
+const JavaScript = styled.div`
+  ${Skill};
+  background-color: #F7DF1E;
+  align-items: center;  
+  display: flex;
+`
+const JavaScriptImg = styled.img`
+ width:10%;
+ margin: 1rem;
+`
+const JavaScriptText = styled.p`
+  color:white;
+  display:inline;
+`
+const TypeScript = styled.div`
+  ${Skill};
+  background-color: #3178C6;
+  align-items: center;  
+  display: flex;
+`
+const TypeScriptImg = styled.img`
+ width:10%;
+ margin: 1rem;
+`
+const TypeScriptText = styled.p`
+  color:white;
+  display:inline;
+`
 
 export default Skills;
