c23e763edcb8da548e6c136a1c6b090efd82d6c4
Wjyeop <wnsduq2178@gmail.com>
2023-08-20 03:55:51 +0900

    ADD EXPERIENCE AND EDUCATION

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -88,13 +88,60 @@ function Skills() {
             <div className="Skills-2">
                 <div className='html'>
                     <div>
-                        <img src='img/html5.png' alt=""/>
-                        <span>HTML5</span>
+                        <img src='img/soongsil.png' style={{borderRadius:"7px"}} alt=""/>
+                        <span>숭실원격평생교육원대학교(4년)</span>
                     </div>                    
                     <ul>
-                        <li>브라우저와의 호환성을 검토하여 개발합니다.</li>
-                        <li>시맨틱 태그를 유용하게 사용하여 코드의 가독성을 높입니다.</li>
-                        <li>localStorage와 sessionStorage를 활용합니다.</li>
+                        <li>23년 6월 ~ 진행 중</li>
+                        <li>컴퓨터공학</li>
+                        <li>데이터베이스, 알고리즘, 네트워크, 운영체제, 멀티미디어개론, 등</li>
+                    </ul>
+                </div>
+                <div className='html'>
+                    <div>
+                        <img src='img/dongyang.jpg' style={{borderRadius:"7px"}} alt=""/>
+                        <span>동양미래대학교(2년)</span>
+                    </div>                    
+                    <ul>
+                        <li>18년 3월 ~ 22년 2월</li>
+                        <li>컴퓨터공학</li>
+                        <li>JAVA, C, PYTHON 등 기본 개발 언어 수강</li>
+                        <li>컴퓨터공학 이론 수강</li>
+                        <li>졸업프로젝트 : 안드로이드 자바 앱 개발</li>
+                    </ul>
+                </div>
+                <div className='html'>
+                    <div>
+                        <img src='img/inflearn.jpg' style={{borderRadius:"7px"}} alt=""/>
+                        <span>인프런 개발자 교육</span>
+                    </div>                    
+                    <ul>
+                        <li>리액트&파이어베이스를 이용한 웹 개발 필수 기능 구현</li>
+                        <li>회원가입, 로그인 기능 구현</li>
+                        <li>게시글 작성 및 저장 삭제 기능 구현</li>
+                        <li>https://react-with-firebase-39fa2.web.app/login</li>
+                    </ul>
+                </div>
+                <div className='html'>
+                    <div>
+                        <img src='img/wanted.png' style={{borderRadius:"7px"}} alt="" />
+                        <span>원티드 개발자 교육</span>
+                    </div>                    
+                    <ul>
+                        <li>TypeScript</li>
+                        <li>TypeScript를 쓰는 이유</li>
+                    </ul>
+                </div>
+                <div className='html'>
+                    <div>
+                        <img src='img/programmers.png' style={{borderRadius:"7px"}} alt=""/>
+                        <span>프로그래머스 개발자 교육</span>
+                    </div>                    
+                    <ul>
+                        <li>FE 채용 과제로 배우는 자바스크립트</li>
+                        <li>React.js, Vue.js의 지식이 되는 JavaScript의 기본기를 익히기 위한 과목으로, VanillaJS (외부 라이브러리/프레임워크를 사용하지 않는 순수 JavaScript)만을 활용해서 웹 기능을 개발하고 유지보수하는 방법에 대한 내용을 공부합니다.</li>
+                        <li>1주차 : 코딩테스트 ≫ 레이아웃,API 요청 ≫ 로딩 UI, 랜덤버튼 ≫ 숫자와 문자열 ≫ 로컬스토리지 ≫ 스크롤과 결과 없음</li>
+                        <li>2주차 : Intersectionobserver ≫ API ≫ 모듈화 ≫ 테스트 ≫ 추가 요구사항 해결하기 ≫ 코드리뷰 ≫ 최종 테스트</li>
                     </ul>
                 </div>
             </div>
